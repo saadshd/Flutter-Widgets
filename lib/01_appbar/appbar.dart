5515dd648426713fdcd87c574b5af52787a47d58
SAADSHD <101456852+SAADSHD@users.noreply.github.com>
2022-08-30 19:38:49 +0500

    Update appbar.dart

--- lib/01_appbar/appbar.dart
+++ lib/01_appbar/appbar.dart
@@ -12,23 +12,20 @@ class _MyAppBarState extends State<MyAppBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('AppBar',
-          style: TextStyle(color: Colors.white),),
-        elevation: 0,
-        backgroundColor: Colors.indigo,
+        title: Text('AppBar'),
         automaticallyImplyLeading: false,
         actions: [
           IconButton(
             onPressed: () {},
-            icon: Icon(Icons.search, color: Colors.white,),
+            icon: Icon(Icons.search),
           ),
           IconButton(
             onPressed: () {},
-            icon: Icon(Icons.notifications_outlined, color: Colors.white,),
+            icon: Icon(Icons.notifications_outlined),
           ),
           IconButton(
             onPressed: () {},
-            icon: Icon(Icons.logout, color: Colors.white,),
+            icon: Icon(Icons.logout),
           ),
         ],
       ),

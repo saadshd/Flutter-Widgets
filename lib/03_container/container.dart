504d3de63ee55d13ca1c0e31e275c45083ab6080
SAADSHD <101456852+SAADSHD@users.noreply.github.com>
2022-08-31 01:17:39 +0500

    Update container.dart

--- lib/03_container/container.dart
+++ lib/03_container/container.dart
@@ -7,7 +7,7 @@ class MyContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Container'),
+        title: const Text('Container'),
       ),
       body: Center(
         child: Column(
@@ -16,7 +16,15 @@ class MyContainer extends StatelessWidget {
             Container(
               height: 200,
               width: 200,
+              decoration: const BoxDecoration(
               color: Colors.indigo,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50),
+                topRight: Radius.circular(5),
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50),
+              )
+              ),
             ),
             Container(
               height: 200,
@@ -27,11 +35,11 @@ class MyContainer extends StatelessWidget {
               boxShadow: [
                 BoxShadow(
                   color: Colors.grey.shade500,
-                  offset: Offset(10.0, 10.0),
+                  offset: const Offset(10.0, 10.0),
                   blurRadius: 25.0,
                   spreadRadius: 2.0,
                 ),
-                BoxShadow(
+                const BoxShadow(
                   color: Colors.white,
                   offset: Offset(-10.0, -10.0),
                   blurRadius: 25.0,

c5588ebdad23db906e4d3af1de7a642cbce49aff
SAADSHD <101456852+SAADSHD@users.noreply.github.com>
2022-09-01 13:42:05 +0500

    Network Image

--- lib/04_image/image.dart
+++ lib/04_image/image.dart
@@ -9,11 +9,19 @@ class MyImage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Image'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image(image: AssetImage('images/logo.png'))
-        ],
+      body: Center(
+        child: Column(
+          children: [
+
+            Text('\n\nAsset Image'),
+            Image(image: AssetImage('images/logo.png')),
+            Text('\n\n\nNetwork Image'),
+            Image(image: NetworkImage('https://images.unsplash.com/photo-1661993749894-7771a581f7de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'),
+            width: 350,
+            height: 300,)
+          ],
+
+        ),
       ),
     );
   }

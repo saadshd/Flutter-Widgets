13a313684883fc087fa54c5c03f3aee6d4dd7f49
SAADSHD <101456852+SAADSHD@users.noreply.github.com>
2022-09-02 23:42:59 +0500

    Update text.dart

--- lib/06_text/text.dart
+++ lib/06_text/text.dart
@@ -22,6 +22,19 @@ class MyText extends StatelessWidget {
           ),
           textAlign: TextAlign.center,
           ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              color: Colors.indigo.shade100,
+              child: Text('Flutter is an open-source UI software development kit created by Google. It is used to develop cross platform applications for Android, iOS, Linux, macOS, Windows, Google Fuchsia, and the web from a single codebase. First described in 2015, Flutter was released in May 2017',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.indigo.shade900,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+          ),
         ],
       ),
     );

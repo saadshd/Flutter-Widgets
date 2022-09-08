caa41cd5491596ffeed86a014473ccaeffb1e32a
SAADSHD <101456852+SAADSHD@users.noreply.github.com>
2022-09-08 22:54:40 +0500

    Update text.dart

--- lib/06_text/text.dart
+++ lib/06_text/text.dart
@@ -7,13 +7,13 @@ class MyText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Text'),
+        title: const Text('Text'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text('Muhammad Saad Shahid'),
-          Text('Practicing and Exploring Flutter Widgets',
+          const Text('Muhammad Saad Shahid'),
+          const Text('Practicing and Exploring Flutter Widgets',
           style: TextStyle(
             fontSize: 25,
             fontStyle: FontStyle.italic,
@@ -25,11 +25,11 @@ class MyText extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Container(
-              color: Colors.indigo.shade100,
-              child: Text('Flutter is an open-source UI software development kit created by Google. It is used to develop cross platform applications for Android, iOS, Linux, macOS, Windows, Google Fuchsia, and the web from a single codebase. First described in 2015, Flutter was released in May 2017',
+              color: Colors.indigo,
+              child: const Text('Flutter is an open-source UI software development kit created by Google. It is used to develop cross platform applications for Android, iOS, Linux, macOS, Windows, Google Fuchsia, and the web from a single codebase. First described in 2015, Flutter was released in May 2017',
                 style: TextStyle(
                   fontSize: 16,
-                  color: Colors.indigo.shade900,
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.justify,
               ),
